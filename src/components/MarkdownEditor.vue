c560eec786a79496a2989d63a20b9b6a4e5c7f06
周超 <zhouchao@zhixueyun.com>
2016-11-29 09:54:19 +0800

    add markdown editor

--- src/components/MarkdownEditor.vue
+++ src/components/MarkdownEditor.vue
@@ -2,12 +2,24 @@
 .markdown
   .markdown-wrapper
     .markdown-tool
-      i(@click="doCode('**')") B
-      i I
-      i(@click="doAction('[link](http://)', 0, -1)") Link
+      span.action-group
+        i.iconfont.icon-bold(@click="doCode('**')" hotkey="ctrl+b")
+        i.iconfont.icon-italic(@click="doCode('*')" hotkey="ctrl+i")
+        i.iconfont.icon-underline(@click="doAction('<u></u>', 3)" hotkey="ctrl+u")
+        i.iconfont.icon-shanchuxian2(@click="doCode('~~')" hotkey="ctrl+d")
+        i.iconfont.icon-chain(@click="doAction('[Type Your Link Name](http://)', -1)" hotkey="ctrl+l")
+        i.iconfont.icon-code(@click="toCode()")
+        i.iconfont.icon-ellipsish(@click="doAction('\\n\\n---\\n\\n', 0, '')")
+        i.iconfont.icon-quoteleft(@click="doAction('\\n> ', -1, '')")
+      span.action-group
+        i.iconfont.icon-mailreply
+        i.iconfont.icon-mailforward
+      span.action-group
+        i.iconfont.icon-compress
+        i.iconfont.icon-expand
     .markdown-content
       .content-wrapper
-        textarea.markdown-editor(ref="editor" v-model="content" spellcheck="false" autocapitalize="off" autocomplete="off" autocorrect="off")
+        textarea.markdown-editor(ref="editor" v-model="content" spellcheck="false" autocapitalize="off" autocomplete="off" autocorrect="off" @keydown="keydown")
         .markdown-preview(v-html="preview")
 </template>
 
@@ -21,12 +33,14 @@ function getEditorSelection (editor) {
   }
 }
 
-function setEditorRange (editor, start, end = start) {
+function setEditorRange (editor, start, length = 0) {
   editor.focus()
-  editor.setSelectionRange(start, end)
+  editor.setSelectionRange(start, start + length)
 }
 
-const markdown = markdownIt()
+const markdown = markdownIt({
+  html: true
+})
 export default {
   data () {
     return {
@@ -34,20 +48,55 @@ export default {
     }
   },
   methods: {
-    doAction () {
-      // TODO this ~~
+    keydown (e) {
+      if (e.ctrlKey === true) {
+        let code = e.key
+        let hotkey = 'ctrl+' + code
+        let el = this.$el.querySelector(`[hotkey='${hotkey}']`)
+        if (el) {
+          e.preventDefault()
+          el.click()
+        }
+      }
     },
-    doCode (code, defaultStr = 'default') {
+    toCode () {
+      let select = this.getSelectStr()
+      let code = '`'
+      if (select.indexOf('\n') > -1) {
+        code = '```'
+      }
+      this.doCode(code)
+    },
+    insertToEditor (actionBefore, actionAfter, defaultStr) {
       let editor = this.$refs.editor
       let {start, end} = getEditorSelection(editor)
-      let beforeStr = this.content.substr(0, start)
-      let selectStr = this.content.substr(start, end - start) || defaultStr
-      let afterStr = this.content.substr(end, this.content.length)
-      selectStr = code + selectStr + code
-      this.content = beforeStr + selectStr + afterStr
+      let {before, select, after} = this.selectedStr(start, end, defaultStr)
+      let newInsert = actionBefore + select + actionAfter
+      this.content = before + newInsert + after
       this.$nextTick(() => {
-        setEditorRange(editor, start + code.length, start + selectStr.length - code.length)
+        setEditorRange(editor, start + actionBefore.length, select.length)
       })
+    },
+    doAction (action, relativeEnd = 0, dstr) {
+      if (relativeEnd <= 0) relativeEnd = action.length + relativeEnd
+      let actionBefore = action.substr(0, relativeEnd)
+      let actionAfter = action.substr(relativeEnd)
+      this.insertToEditor(actionBefore, actionAfter, dstr)
+    },
+    doCode (code, dstr) {
+      this.insertToEditor(code, code, dstr)
+    },
+    getSelectStr () {
+      let editor = this.$refs.editor
+      let {start, end} = getEditorSelection(editor)
+      let {select} = this.selectedStr(start, end)
+      return select
+    },
+    selectedStr (start, end, defaultStr = 'default') {
+      let before = this.content.substr(0, start)
+      let select = this.content.substr(start, end - start) || defaultStr
+      let after = this.content.substr(end, this.content.length)
+      return {before, select, after}
     }
   },
   computed: {
@@ -59,21 +108,26 @@ export default {
 </script>
 
 <style lang="less">
+@import "./styles/iconfont/iconfont.css";
 .markdown-editor{
   border: 0;
   outline: 0;
   resize: none;
   border-right: 1px solid #eee;
 }
+.markdown-tool{
+  padding: 10px;
+  .action-group{
+    margin-right: 25px;
+  }
+  .iconfont{
+    font-size: 1.5em;
+    padding: 15px;
+  }
+}
 .markdown{
   background: #eee;
   height: 100%;
-  .markdown-tool{
-    padding: 10px;
-    &>i{
-      margin-right: 10px;
-    }
-  }
   .markdown-wrapper{
     position: relative;
     height: 100%;
@@ -89,6 +143,7 @@ export default {
       width: 100%;
       display: flex;
       &>*{
+        position: relative;
         flex: 1;
         box-sizing: border-box;
         padding: 10px;
