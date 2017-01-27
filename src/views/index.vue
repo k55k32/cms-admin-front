e85571cb790da05483d9165575ebbb8455e8a3e1
Diamond <diamondfsd@gmail.com>
2017-01-27 10:00:41 +0800

    add menu toggle

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,8 +1,10 @@
 <template lang="pug">
 el-row.index-wrapper
-  el-dialog(title="编辑", v-model="editDialog")
+  el-dialog(title="用户编辑", v-model="editDialog")
     edit(:data="editData" @save="save" v-loading="saveLoading")
-  el-col.nav(:span="4")
+  .menu-toggle(@click="menuShowToggle")
+    i.fa.fa-bars
+  el-col.nav(:span="4", v-show="menuShow")
     el-menu.nav-menu(theme='dark' , :default-active = "defaultItem.id" , :default-openeds="[defaultItem.parentId]" ,:router="true")
       .header-logo
         h2 Manager
@@ -13,7 +15,7 @@ el-row.index-wrapper
             i.el-icon-message
             | {{item.text}}
           el-menu-item(v-for="(child, index) in item.children", :index="child.id" , :route="{name: child.name}") {{child.text}}
-  el-col.content(:span="20")
+  el-col.content(:span="menuShow ? 20 : 24")
     .content-header
       header
         h2.title {{defaultItem.text || $route.title}}
@@ -46,13 +48,22 @@ export default {
   created () {
     this.$router.beforeEach((to, from, next) => {
       this.loadContent = true
+      if (this.minScreen) {
+        this.menuShow = false
+      }
       next()
     })
     this.$router.afterEach(route => {
       this.loadContent = false
     })
+    if (this.minScreen) {
+      this.menuShow = false
+    }
   },
   methods: {
+    menuShowToggle () {
+      this.menuShow = !this.menuShow
+    },
     showEdit () {
       this.editDialog = true
     },
@@ -67,10 +78,14 @@ export default {
       lastItem: {},
       loadContent: false,
       editDialog: false,
-      editData: { username: this.$store.state.user.username, password: '' }
+      editData: { username: this.$store.state.user.username, password: '' },
+      menuShow: true
     }
   },
   computed: {
+    minScreen () {
+      return window.screen.width < 768
+    },
     defaultItem () {
       let name = this.$route.name
       let defaultItem = {}
@@ -152,6 +167,29 @@ export default {
   .operator{
     display: inline-block;
     cursor: pointer;
+  }
+}
+@toggle-index: 999;
+.menu-toggle{
+  display: none;
+  font-size: 25px;
+  position: fixed;
+  bottom: 15px;
+  left: 15px;
+  z-index: @toggle-index;
+}
+@media all and (max-width: 768px) {
+  .menu-toggle{
+    display: block;
+  }
+  .index-wrapper .nav{
+    position: fixed;
+    width: 100%;
+    z-index: @toggle-index - 1;
+    height: 100%;
+    top: 0;
+    left: 0;
+    overflow: auto;
   }
 }
 </style>
